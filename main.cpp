a58351d8278842d5c1208c5f993e6c2fa6678fbf
Никита <forgofaceit@gmail.com>
2024-12-19 21:03:56 +0300

    Добавлен ввод двух чисел с выводом их суммы

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include "sdt.h"
 
 int main() {
+    int a, b;
+
+    std::cout << "Введите первое число: ";
+    std::cin >> a;
+
+    std::cout << "Введите второе число: ";
+    std::cin >> b;
+
+    std::cout << "Сумма чисел: " << a + b << std::endl;
+
     return 0;
 }
+
