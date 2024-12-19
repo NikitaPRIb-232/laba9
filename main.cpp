2f65dcfb3ffccff6e9d93ba4f59a048f90e58356
Никита <forgofaceit@gmail.com>
2024-12-19 22:45:04 +0300

    умножение

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main() {
 
     cout << "Сумма чисел: " << a + b << endl;
     cout << "Разность чисел: " << a - b << endl;
-    cout << "Деление чисел: " << a \ b << endl;
+    cout << "Умножение чисел: " << a * b << endl;
 
     return 0;
 }
