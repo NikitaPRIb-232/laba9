a778f5c260dda12d40d24ad83770cc90b952993f
Никита <forgofaceit@gmail.com>
2024-12-19 22:40:42 +0300

    изменения

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main() {
 
     cout << "Сумма чисел: " << a + b << endl;
     cout << "Разность чисел: " << a - b << endl;
-    cout << "Произведение чисел: " << a * b << endl;
+    cout << "Деление чисел: " << a \ b << endl;
 
     return 0;
 }
